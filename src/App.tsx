35f9b9d8999194ae68837b1419b590075c2a20cc
Steve Saju <stevesajustephen@gmail.com>
2025-11-14 17:51:57 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChatBotStart from "./components/ChatBotStart";
+
+function App() {
+  return (
+    <div className="container">
+      <ChatBotStart />
+    </div>
+  );
+}
+
+export default App;
