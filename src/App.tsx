b8090120a51c33984b3ffde57cc08f613306fe43
Steve Saju <stevesajustephen@gmail.com>
2025-11-20 11:00:00 +0100

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ChatBotApp from "./components/ChatBotApp";
 import ChatBotStart from "./components/ChatBotStart";
 
 function App() {
   return (
     <div className="container">
-      <ChatBotStart />
+      {/* <ChatBotStart /> */}
+      <ChatBotApp />
     </div>
   );
 }
---
+++ src/components/ChatBotApp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ChatBotApp = () => {
+  return (
+    <div className="chat-app">
+      <div className="chat-list">
+        <div className="chat-list-header">
+          <h2>Chat List</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBotApp;
